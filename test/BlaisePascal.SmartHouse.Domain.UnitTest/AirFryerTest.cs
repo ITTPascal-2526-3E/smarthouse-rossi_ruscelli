5e5bc51c055f859548fda8a12ff4afc03a29dddc
Rusceee <ruscelli.andreas@gmail.com>
2025-12-01 10:52:05 +0100

    aggiunte le classi test

---
+++ test/BlaisePascal.SmartHouse.Domain.UnitTest/AirFryerTest.cs
@@ -0,0 +1,18 @@
+﻿using BlaisePascal.SmartHouse.Domain.AirFryer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.UnitTest
+{
+    public class AirFryerTest
+    {
+        [Fact]
+        public void ConstructorAndProperties_ShouldInitializeCorrectly()
+        {
+                AirFryer air = new AirFryer();
+        }
+    }
+}
---
+++ test/BlaisePascal.SmartHouse.Domain.UnitTest/HeatPumpTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.Heat_Pump;
+
+namespace BlaisePascal.SmartHouse.Domain.UnitTest
+{
+    internal class HeatPumpTest
+    {
+    }
+}
