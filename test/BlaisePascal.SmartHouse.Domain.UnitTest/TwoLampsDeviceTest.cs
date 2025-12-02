fe0997a0b21f1eec0703839ad1cf70aa1330c47a
Rusceee <ruscelli.andreas@gmail.com>
2025-12-02 13:45:09 +0100

    aggiunti i primi test di twolampsdevice da fixare

--- test/BlaisePascal.SmartHouse.Domain.UnitTest/TwoLampsDeviceTest.cs
+++ test/BlaisePascal.SmartHouse.Domain.UnitTest/TwoLampsDeviceTest.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using BlaisePascal.SmartHouse.Domain.Lamps;
-using Xunit;
 
 namespace BlaisePascal.SmartHouse.Domain.UnitTest
 {
     public class TwoLampsDeviceTest
     {
         [Fact]
-        public void PlaceholderTest()
+        public void ConstructorAndProperties_ShouldInitializeCorrectly()
         {
-            // Ensure this test project compiles; actual tests can be added later.
-            Assert.True(true);
+            // Arrange
+            public Lamp lamp1 = new Lamp(true, "temporanea", ColorType.Blue, 10, LampType.Induction);
+
+            public EcoLamp lamp2 = new EcoLamp(true, "temporanea2", ColorType.Blue, 10, LampType.Induction);
+           
+            // Act
+            public TwoLampDevice twoLampsDevice = new TwoLampDevice(lamp1, lamp2);
+            // Assert
+            Assert.Equal(lamp1, twoLampsDevice.Lamp);
+            Assert.Equal(lamp2, twoLampsDevice.EcoLamp);
+        }
+        [Fact]
+        public void TurnOffBothLamps_ShouldUpdateTheirStatus()
+        {
+            // Arrange
+            public Lamp lamp1 = new Lamp(true, "temporanea2", ColorType.Blue, 10, LampType.Induction);
+
+            public EcoLamp lamp2 = new EcoLamp(true, "temporanea2", ColorType.Blue, 10, LampType.Induction);
+        // Act
+        var twoLampsDevice = new TwoLampsDevice(lamp1, lamp2);
+            twoLampsDevice.TurnOffBoth();
+            // Assert
+
         }
     }
 }
